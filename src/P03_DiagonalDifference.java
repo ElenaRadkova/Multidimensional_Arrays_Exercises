8259618b51971cc6d5d21ea3c6629f6a9ef28838
ElenaRadkova <122015420+ElenaRadkova@users.noreply.github.com>
2024-01-12 22:28:04 +0200

    Add solution of the exercise

--- src/P03_DiagonalDifference.java
+++ src/P03_DiagonalDifference.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class P03_DiagonalDifference {
+import java.util.Scanner;
+
+public class P03_DiagonalDifference {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int [][] matrix = new int[n][n];
+
+        //1. пълним матрицата
+        fillMatrix(matrix, scanner);
+        //2. сума на главен диагонал
+        int primarySum = primaryDiagonalSum(matrix);
+        //3. сума на второстепенен диагонал
+        //формула за второстепенен диагонал -> col == rows (редовете) - row (текущия ред) - 1;
+        int secondarySum = secondaryDiagonalSum(matrix);
+        //4. абсолютна стойност на двете суми -
+        int difference = Math.abs(primarySum - secondarySum);
+        System.out.println(difference);
+    }
+
+    private static int secondaryDiagonalSum(int[][] matrix) {
+        int sum = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                int currentElement = matrix[col][row];
+                if (col == matrix.length - row - 1) {
+                    sum+= currentElement;
+                }
+            }
+        }
+        return sum;
+    }
+
+    private static int primaryDiagonalSum(int[][] matrix) {
+        int sum = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                int currentElement = matrix[col][row];
+                if (row == col) {
+                    sum+= currentElement;
+                }
+            }
+        }
+        return sum;
+    }
+
+    private static void fillMatrix(int[][] matrix, Scanner scanner) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+    }
 }
