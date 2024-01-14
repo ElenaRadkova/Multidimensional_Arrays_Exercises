40fc626da21ba90b074ee832a5dbd456af10ddaa
ElenaRadkova <122015420+ElenaRadkova@users.noreply.github.com>
2024-01-14 13:30:55 +0200

    Add solution of the exercise

--- src/P02MatrixOfPalindromes.java
+++ src/P02MatrixOfPalindromes.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class P02MatrixOfPalindromes {
+import java.util.Scanner;
+
+public class P02MatrixOfPalindromes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+
+        String [][] matrix = new String[rows][cols];
+
+        char startLetter = 'a';
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                String palindrome = ""+ startLetter + (char)(startLetter + col) + startLetter; //конкатенираме с празен стринг, за да може символите от тип char да го приеме като стринг
+                matrix[row][col] = palindrome;
+            }
+            startLetter = (char)(startLetter + 1);// преди да премине на следващия ред, увеличава буквата с едно
+        }
+        printMatrix(matrix, rows, cols);
+    }
+
+    private static void printMatrix(String[][] matrix, int rows, int cols) {
+        for (int col = 0; col < matrix.length; col++) {
+            for (int row = 0; row < matrix[col].length; row++) {
+                System.out.print(matrix[col][row] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
