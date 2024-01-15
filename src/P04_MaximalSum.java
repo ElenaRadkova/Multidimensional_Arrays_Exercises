05db336a236d99e2cc94cb72d2bfcf9cd4112221
ElenaRadkova <122015420+ElenaRadkova@users.noreply.github.com>
2024-01-15 18:00:26 +0200

    Add solution of the exercise

--- src/P04_MaximalSum.java
+++ src/P04_MaximalSum.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class P04_MaximalSum {
+import java.util.Scanner;
+
+public class P04_MaximalSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+
+        int [][] matrix = new int[rows][cols];
+          fillMatrix(matrix, rows, cols, scanner);
+
+        int maxSum = Integer.MIN_VALUE;
+        int bestStartingRow = 0;
+        int bestStartingCol = 0;
+        for (int row = 0; row < rows - 2; row++) {
+            for (int col = 0; col < cols - 2; col++) {
+                int sum = matrix[row][col] + matrix[row][col + 1] + matrix[row][col + 2]
+                        + matrix[row + 1][col] + matrix[row + 1][col + 1] + matrix[row + 1][col + 2]
+                        + matrix[row + 2][col] + matrix[row + 2][col + 1] + matrix[row + 2][col + 2];
+
+                if(sum > maxSum){
+                    maxSum = sum;
+                    bestStartingRow = row;
+                    bestStartingCol = col;
+                }
+            }
+        }
+        System.out.println("Sum = " + maxSum);
+        for (int row = bestStartingRow; row < bestStartingRow + 3; row++) {
+            for (int col = bestStartingCol; col < bestStartingCol + 3; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+    private static void fillMatrix(int[][] matrix, int rows, int cols, Scanner scanner) {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+    }
+
 }
